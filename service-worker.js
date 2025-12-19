2cccacaf52459c49986596b2b547aec69ec1400e
francescoizzo07-oss <francesco.izzo07@gmail.com>
2025-12-19 23:37:37 +0100

    Refactor service worker for improved caching
    Updated service worker to use a new cache name and added caching for additional files.

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,43 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("quiz-cache").then(cache =>
-      cache.addAll(["./"])
-    )
+const CACHE_NAME = "quiz-farmacia-v2";
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json"
+      ]);
+    })
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(k => {
+          if (k !== CACHE_NAME) {
+            return caches.delete(k);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, clone);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
